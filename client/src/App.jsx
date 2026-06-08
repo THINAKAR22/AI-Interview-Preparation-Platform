e50b191ff01d1859e77b2f7591c83ebb086fcaa2
THINAKAR22 <thinakark51@gmail.com>
2026-06-08 21:21:44 +0530

    Refactor Dashboard component: Enhance UI with new charts, activity tracking, and stat cards; streamline data handling and improve layout.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import Dashboard from "./pages/Dashboard";
+import Dashboardnew from "./pages/Dashboard_new";
 
 export default function App() {
   
   return (
     <>
-    <Dashboard />
+    <Dashboardnew />
     </>
   )
 
